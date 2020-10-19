fbc4389b96f5d4ba32b8018e79d34d6b5cdd1c05
MannDP <patelmann1999@gmail.com>
2020-10-18 21:17:59 -0400

    left sidebar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Header from './Header';
+import Sidebar from "./Sidebar";
 import './App.css';
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      {/* Body */}
-        {/* Sidebar */}
-        {/* Feed */}
-          {/* Stories */}
-          {/* Post */}
-          {/* Messages */}
-        {/* Widgets */}
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header/>
+            <div className="app__body">
+                <Sidebar />
+                {/* Feed */}
+                {/* Stories */}
+                {/* Post */}
+                {/* Messages */}
+                {/* Widgets */}
+            </div>
+        </div>
+    );
 }
 
 export default App;
